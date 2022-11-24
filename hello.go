ebbf03060cd27e14693909bf4f52e1dda5abb97c
Alan Calvillo <alan.calvillov@AlanCalvillov.local>
2022-11-23 21:14:16 -0600

    first class

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const englishGreeting = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishGreeting + name
+}
+
+func main() {
+	fmt.Println(Hello("Alan"))
+}
