3d7d7950a123ece1209a6cd9dd47338afb774f0a
Alan Calvillo <alan.calvillov@AlanCalvillov.local>
2022-11-23 21:14:16 -0600

    first class

---
+++ hello_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T){
+
+	t.Run("Greeting people", func(t *testing.T){	
+		actual := Hello("Alan")
+		expected := "Hello, Alan"
+
+		if actual != expected {
+			t.Errorf("actual %q expected %q",actual,expected)
+		} 
+	})
+
+	t.Run("say 'Hello, World when an empty string is supplied", func(t *testing.T){
+		actual := Hello("")
+		expected := "Hello, World"
+
+		if actual != expected{
+			t.Errorf("actual %q expected %q",actual, expected)
+		}
+	})
+}
+
+func assertCorrectMessage(t testing.TB, actual, expected string){
+	t.Helper()
+	if actual != expected {
+		t.Errorf("actual %q expected %q", actual, expected)
+	}
+}
+
